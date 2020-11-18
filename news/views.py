90c06bd07ab6eac344dbb9f5a272843876e366fa
CriGrolla <crygro02@gmail.com>
2020-11-18 13:18:46 +0100

    L15

--- news/views.py
+++ news/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    a=""
-    g=""
+    a=[]
+    g=[]
     for art in Articolo.objects.all():
-        a+=(art.titolo+"<br>")
+        a.append(art.titolo)
 
     for gio in Giornalista.objects.all():
-         g+=(gio.nome+"<br>")
-    response="Articoli:<br>"+a+"<br>Giornalisti:<br>"+g
-    return HttpResponse("<h1>Homepage news!</h1>")
+        g.append(gio.nome)
+
+    response=str(a)+"<br>"+str(g)
+    print(response)
+    return HttpResponse("<h1>"+response+"</h1>")
