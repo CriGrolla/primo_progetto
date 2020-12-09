81c772cac1f44428667623749e8a5ca905259687
CriGrolla <crygro02@gmail.com>
2020-12-09 10:31:44 +0100

    L18

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, articoloDetailView
+from .views import home, articoloDetailViewCB #articoloDetailView
 
 app_name='news'
 
 urlpatterns=[
     path("", home, name="homeview"),
-    path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
+    #path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
+    path("articoli/<int:pk>", articoloDetailViewCB.as_view(), name="articolo_detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -12,3 +12,7 @@ def articoloDetailView(request, pk):
     articolo=get_object_or_404(Articolo, pk=pk)
     context = {"articolo": articolo}
     return render(request, "articolo_detail.html", context)
+
+def articoloDetailViewCB(DetailView):
+    model=Articolo
+    template_name= "articolo_detail.html"
